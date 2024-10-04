3df947fd3f33029cf59a891073fe9dd79c196748
zoryamba <zoryamba@gmail.com>
2024-10-05 01:00:53 +0300

    run leptosfmt

--- src/components/input.rs
+++ src/components/input.rs
@@ -5,12 +5,13 @@ pub fn Input(initial_text: String, set_input: WriteSignal<String>) -> impl IntoV
     view! {
         <div class="w-full border border-gray-200 rounded-lg  dark:border-blue-300">
             <div class="px-4 py-2 bg-white rounded-t-lg h-96">
-                <textarea rows="4"
-                          class="h-80 outline-none w-full px-0 text-sm text-gray-900 bg-white border-0 resize-none focus:ring-0 dark:text-dark dark:placeholder-gray-400"
-                          placeholder="Write a comment..."
-                          on:input=move |ev| { set_input.set(event_target_value(&ev)) }
+                <textarea
+                    rows="4"
+                    class="h-80 outline-none w-full px-0 text-sm text-gray-900 bg-white border-0 resize-none focus:ring-0 dark:text-dark dark:placeholder-gray-400"
+                    placeholder="Write a comment..."
+                    on:input=move |ev| { set_input.set(event_target_value(&ev)) }
                 >
-                    { initial_text }
+                    {initial_text}
                 </textarea>
             </div>
         </div>
--- src/components/tabs.rs
+++ src/components/tabs.rs
@@ -12,19 +12,19 @@ pub fn Tabs(tabs: IndexMap<&'static str, View>) -> impl IntoView
     for (i, (key, value)) in tabs.into_iter().enumerate() {
         tab_heads.push(view! {
             <li class="me-2" on:click=move |_| set_active.set(i)>
-                <a href="#"
-                   class="inline-block p-4 border-b-2 rounded-t-lg tab"
-                   class=("active", move || active.get() == i)
-                >{ key }</a>
+                <a
+                    href="#"
+                    class="inline-block p-4 border-b-2 rounded-t-lg tab"
+                    class=("active", move || active.get() == i)
+                >
+                    {key}
+                </a>
             </li>
         });
 
         tab_contents.push(view! {
-            <div
-                class="justify-center mt-8"
-                class=("hidden", move || active.get() != i)
-            >
-                { value }
+            <div class="justify-center mt-8" class=("hidden", move || active.get() != i)>
+                {value}
             </div>
         });
     }
@@ -32,14 +32,10 @@ pub fn Tabs(tabs: IndexMap<&'static str, View>) -> impl IntoView
     view! {
         <section>
             <div class="mb-2 text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
-                <ul class="flex flex-wrap -mb-px">
-                    { tab_heads }
-                </ul>
+                <ul class="flex flex-wrap -mb-px">{tab_heads}</ul>
             </div>
         </section>
 
-        <section>
-            { tab_contents }
-        </section>
+        <section>{tab_contents}</section>
     }
 }
